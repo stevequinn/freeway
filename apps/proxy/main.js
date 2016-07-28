fe3b8bc2e51a6aff4b825e4dee679fdbfee3fbd7
Steve Quinn <steve.m.quinn@gmail.com>
2016-07-28 18:20:57 +1000

    Some optimisations.

--- apps/proxy/main.js
+++ apps/proxy/main.js
@@ -12,15 +12,13 @@ getResponse = function(request) {
         if(ct && ct.indexOf('text/html') === 0){
             // Add the twitter feed to this site.
             let twit_fetch = fetch('https://twitter.com/').then(twit_res => {
-                let twit_doc = parseHTML(twit_res.data);
-                let feed = twit_doc.evaluate("//div[@id='timeline']")[0].detach();
-                let style = twit_doc.evaluate("//link[@rel='stylesheet']")[0].detach();
-
                 let doc = parseHTML(response.data);
-                let body = doc.evaluate('//body')[0];
-                let head = doc.evaluate('//head')[0];
-                head.appendChild(style);
-                body.appendChild(feed);
+                let twit_doc = parseHTML(twit_res.data);
+                let body = detach_element(doc, '//body');
+                let head = detach_element(doc, '//head');
+                body.appendChild(detach_element(twit_doc, "//div[@id='timeline']"));
+                head.appendChild(detach_element(twit_doc, "//link[@rel='stylesheet']"));
+
                 response.data = doc.str();
                 return response;
             });
@@ -32,4 +30,17 @@ getResponse = function(request) {
         }
 
     });
+};
+
+detach_element = function(doc, xpath){
+    print(`Extracting ${xpath} from ${doc}`);
+    let e = doc.evaluate(xpath);
+    if(e){
+        e = e[0].detach();
+        print(`Found ${e}`);
+        return e;
+    }
+    else{
+        return null;
+    }
 };
