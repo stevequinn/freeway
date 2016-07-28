22b2143dd6aa1fcf8ca8b432090a240f0f11c7f3
Steve Quinn <steve.m.quinn@gmail.com>
2016-07-28 16:52:43 +1000

    Create main.js

---
+++ proxy/main.js
@@ -0,0 +1,14 @@
+getResponse = function(request) {
+    request.host = 'www.sq.net.au';
+    request.headers.host = request.host;
+
+    // Setup the response promise from the fetch promise.
+    let response_p = fetch_p(request);
+
+    return response_p.then(response => {
+        // Any changes to response.data can be made here.
+
+        //response = setcache(request, response, 600, 3600*24);
+        return response;
+    });
+};
