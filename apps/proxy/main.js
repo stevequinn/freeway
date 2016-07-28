8288130a63275beaa2ef6b1b94af8827b79b7ae4
Steve Quinn <steve.m.quinn@gmail.com>
2016-07-28 17:53:13 +1000

    Makse the demo a little more interesting by adding twitter feed.

--- apps/proxy/main.js
+++ apps/proxy/main.js
@@ -9,7 +9,15 @@ getResponse = function(request) {
         // Any changes to response.data can be made here.
         let ct = response.headers['content-type'];
         if(ct && ct.indexOf('text/html') === 0){
-            response.data = response.data.replace(/steve/gi, 'quinn');
+            // Add the twitter feed to this site.
+            let twit_fetch = fetch('https://twitter.com/').then(twit_res => {
+                let twit_doc = parseHTML(twit_res.data);
+                let feed = twit_doc.evaluate("//div[@id='timeline']").detach();
+
+                let doc = parseHTML(response.data);
+                doc.evaluate('//body')[0].appendChild(feed);
+                response.data = doc.str();
+            });
         }
         //response = setcache(request, response, 600, 3600*24);
         return response;
